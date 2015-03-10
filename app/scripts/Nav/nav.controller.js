471edc361b85833da507417f4ed3798aff8394c6
John Albritton <jtareb1@att.net>
2015-03-09 21:06:45 -0400

    first commit

---
+++ app/scripts/Nav/nav.controller.js
@@ -0,0 +1,32 @@
+;(function () {
+
+
+	'use strict';
+
+
+	angular.module('BeerFinder')
+
+	.controller('NavCtrl', ['$scope', 'UserFactory',
+
+		function ($scope, UserFactory) {
+
+			var user = UserFactory.user();
+
+			   if (user) {
+        		$scope.loggedin = true;
+        		$scope.user = user;
+      				} else {
+        		$scope.loggedin = false;
+      			}
+
+
+      			$scope.logout = function () {
+        			UserFactory.logout();
+      			};  
+
+
+    }
+
+  ])
+
+}());
