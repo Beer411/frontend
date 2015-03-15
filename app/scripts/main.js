499ba1439b9a95c6744b8bad0da266575952e435
John Albritton <jtareb1@att.net>
2015-03-15 15:29:34 -0400

    items

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,15 +38,27 @@
 
           //Beers list
           .when('/beers', {
-            templateUrl: '/scripts/lists/lists.tpl.html',
+            templateUrl: '/scripts/lists/lists.Beers.tpl.html',
             controller: 'ListCtrl'
           })
 
           //Bars list
           .when('/bars', {
-            templateUrl: '/scripts/lists/lists.tpl.html',
+            templateUrl: '/scripts/lists/lists.Bars.tpl.html',
             controller: 'ListCtrl'
           })
+
+          //add item to Beers list
+          //.when('/beers', {
+          // templateUrl: '/scripts/items/items.beers.tpl.html',
+          //  controller: 'ItemsCtrl'
+          //})
+
+          //add items to Bars list
+          //.when('/bars', {
+          //  templateUrl: 'scripts/items/items.bars.tpl.html',
+          //  controller: 'ItemsCtrl'
+          //})
          
         // Go Home ET
         .otherwise('/');
